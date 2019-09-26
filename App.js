850d0dfa5dc95bad06a0ef9583132848c426505f
丸尾拓馬 <TakumaMaruo@maruotakuma-no-MacBook-Pro.local>
2019-09-26 21:54:23 +0900

    add icon font

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  render(){
   return (
     <View style={styles.container}>
       <Text>Hello</Text>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
